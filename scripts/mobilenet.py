a3bd8645a427bef2efd5ee80e9b9023b3782c581
Tomos Fearn <tfearn10@outlook.com>
2020-01-24 15:01:17 +0000

    working mobilenet python

--- scripts/mobilenet.py
+++ scripts/mobilenet.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
 import rospy
-from sensor_msgs.msg import Image
 import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import os
 
-#import context
+model = cv2.dnn.readNetFromTensorflow('/home/tomos/ros/wheelchair/catkin_ws/src/mobilenet/scripts/models/frozen_inference_graph.pb',
+                                        '/home/tomos/ros/wheelchair/catkin_ws/src/mobilenet/scripts/models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
 
-
-# Pretrained classes in the model
 classNames = {0: 'background',
               1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
               7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
@@ -28,77 +31,83 @@ classNames = {0: 'background',
               86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
 
 
+
 def id_class_name(class_id, classes):
-    for key, value in classes.items():
-        if class_id == key:
-            return value
+  for key, value in classes.items():
+    if class_id == key:
+      return value
 
-#rospy.init_node('mobilenet', anonymous=True)
-print (cv2.__version__)
+class image_converter:
 
-model = cv2.dnn.readNetFromTensorflow('/home/tomos/ros/wheelchair/catkin_ws/src/mobilenet/scripts/models/frozen_inference_graph.pb',
-                                        '/home/tomos/ros/wheelchair/catkin_ws/src/mobilenet/scripts/models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+  def __init__(self):
+    self.image_pub = rospy.Publisher("wheelchair_robot/mobilenet_image",Image, queue_size=10)
 
-cap = cv2.VideoCapture(-1) #0 is laptop 1 is external
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("wheelchair_robot/left_camera/image_raw",Image,self.callback)
+    #self.image_sub = rospy.Subscriber("/zed_node/left_raw/image_raw_color", Image, self.callback)
 
-rosimg = rospy.Subscriber("wheelchair_robot/left_camera", Image)
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
 
+    #(rows,cols,channels) = cv_image.shape
+    #if cols > 60 and rows > 60 :
+    #  cv2.circle(cv_image, (50,50), 10, 255)
 
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+    #cv2.imshow("Image window", cv_image)
+    c = cv2.waitKey(1)
+    if c == 27: #this is the escape key
+      cv2.destroyAllWindows()
 
-#def detectcaller() :
-#  print ("new function")
-  
+    #add code for object classification
+    # Loading model
+    #model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
+    #                                      'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+    #image = cv2.imread("generate.jpg")
 
+    image = cv2.resize(cv_image, (0,0), fx=1.0, fy=1.0) 
 
-while True:
-    
+    image_height, image_width, _ = image.shape
 
-    
-    cv_image = bridge.imgmsg_to_cv2(rosimg, desired_encoding="passthrough")
-    #success, image = rospy.Subscriber("wheelchair_robot/left_camera", Image)
-    success, image = cap.read()
-    if success:
-        #cv2.imwrite(os.path.join('', 'generate.jpg'), image)
-
-        c = cv2.waitKey(1)
-        if c == 27: #this is the escape key
-            break
-
-        #add code for object classification
-        # Loading model
-        #model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
-        #                                      'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
-        #image = cv2.imread("generate.jpg")
-
-        image = cv2.resize(image, (0,0), fx=1.5, fy=1.5) 
-
-        image_height, image_width, _ = image.shape
-
-        model.setInput(cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True))
-        output = model.forward()
+    model.setInput(cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True))
+    output = model.forward()
 
 
-        for detection in output[0, 0, :, :]:
-            confidence = detection[2]
-            if confidence > .3:
-                class_id = detection[1]
-                class_name=id_class_name(class_id,classNames)
-                print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
-                box_x = detection[3] * image_width
-                box_y = detection[4] * image_height
-                box_width = detection[5] * image_width
-                box_height = detection[6] * image_height
-                cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
-                cv2.putText(image,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.002*image_width),(0, 0, 255))
-
-        cv2.imshow('image', image)
-        #rospy.spin()
-    else:
-        break
+    for detection in output[0, 0, :, :]:
+        confidence = detection[2]
+        if confidence > .3:
+            class_id = detection[1]
+            class_name=id_class_name(class_id,classNames)
+            print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
+            box_x = detection[3] * image_width
+            box_y = detection[4] * image_height
+            box_width = detection[5] * image_width
+            box_height = detection[6] * image_height
+            cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
+            cv2.putText(image,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.002*image_width),(0, 0, 255))
+    #cv2.imshow('image', image)
 
 
-cap.release()
-cv2.destroyAllWindows()
+
+
+
+    #cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('mobilenet_ROSit_2_OCV', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
--- scripts/ocv.py
+++ scripts/ocv.py
@@ -43,7 +43,8 @@ class image_converter:
     self.image_pub = rospy.Publisher("image_topic_2",Image, queue_size=10)
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("wheelchair_robot/left_camera/image_raw",Image,self.callback)
+    #self.image_sub = rospy.Subscriber("wheelchair_robot/left_camera/image_raw",Image,self.callback)
+    self.image_sub = rospy.Subscriber("/zed_node/left_raw/image_raw_color", Image, self.callback)
 
   def callback(self,data):
     try:
